10db41d20969ebc68719e6667db4accc3715dc59
Evan Coakley <evancoakley1@gmail.com>
2017-09-20 13:49:25 -0500

    lists added

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -1,8 +1,36 @@
 package com.example;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        List<Student> studentlist = new ArrayList<>();
+
+        studentlist.add(new Student("Sally", "Fields",1, 1));
+        studentlist.add(new Student("Englebert", "Humperdink",2,1));
+        studentlist.add(new Student("Rick", "James", 3,1));
+        studentlist.add(new Student("Zbigniew", "Brzezinski", 4, 1));
+        studentlist.add(new Student("Milton", "Friedman", 5, 1));
+        studentlist.add(new Student("John","Keynes", 6, 1));
+        studentlist.add(new Student("Dante", "Alighieri", 7, 1));
+        studentlist.add(new Student("Leon", "Trotsky", 8,1));
+        studentlist.add(new Student("John", "Milton", 9, 1));
+        studentlist.add(new Student("Duke", "Ellington", 10, 1));
+        studentlist.add(new Student("Miles", "Davis", 11, 1));
+        studentlist.add(new Student("Tyler", "Durden", 12,1));
+        studentlist.add(new Student("Napoleon", "Bonaparte", 13, 1));
+        studentlist.add(new Student("Harriet", "Stowe", 14, 1));
+        studentlist.add(new Student("Emily", "Bronte", 15, 1));
+
+        List<Teacher>teacherList = new ArrayList<>();
+
+        teacherList.add(new Teacher("John", "Keating", 1, 1));
+        teacherList.add(new Teacher("Louanne", "Johnson", 2, 1));
+        teacherList.add(new Teacher("Maurice", "Phipps", 3, 1));
+
+        
+
     }
 }
--- src/com/example/Teacher.java
+++ src/com/example/Teacher.java
@@ -1,15 +1,14 @@
 package com.example;
 
-public class Student {
-
+public class Teacher {
     private String firstName;
     private String lastName;
     private int id;
     private int grade;
 
-    public Student() {}
+    public Teacher() {}
 
-    public Student(String firstName, String lastName, int id, int grade) {
+    public Teacher(String firstName, String lastName, int id, int grade) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.id = id;
@@ -49,23 +48,4 @@ public class Student {
         this.grade = grade;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Student student = (Student) o;
-
-        return id == student.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return id;
-    }
-
-    @Override
-    public String toString() {
-        return firstName +" "+ lastName +", " + id + " " + grade + ".";
-    }
 }
